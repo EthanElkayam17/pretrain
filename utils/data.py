4be23528fedcb4b2b9daa476227bf0102ab061f0
Ethan Elkayam <ethanelkaim17@gmail.com>
2025-01-12 14:00:17 +0200

    a

--- utils/data.py
+++ utils/data.py
@@ -94,8 +94,8 @@ class RexailDataset(datasets.VisionDataset):
 
         if load_into_memory:
             data_shape_sample = (self.__getitem__(0,only_pre_transform=(self.pre_transform is not None)))[0].shape
-            print(data_shape_sample)
-            self.data = torch.empty((len(self.samples), *data_shape_sample), dtype=torch.float16)
+
+            self.data = torch.zeros((len(self.samples), *data_shape_sample), dtype=torch.float16, pin_memory=True)
             self.data.share_memory_()
 
             self._load_everything(num_workers=num_workers)
