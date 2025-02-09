c7c931161582ac0cf75a77197bb63b167689aa8d
Ethan Elkayam <ethanelkaim17@gmail.com>
2025-02-09 15:44:00 +0200

    a

--- engine/trainer.py
+++ engine/trainer.py
@@ -81,8 +81,6 @@ def train_step(model: torch.nn.Module,
 
     train_loss, train_accuracy = 0 , 0
     for batch, (X,y) in enumerate(dataloader):
-        print(X.shape)
-        print(X.dtype)
         X, y = X.to(rank) , y.to(rank)
 
         optimizer.zero_grad()
