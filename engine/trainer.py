b2c2af5e82463af48b5bda8acad3002bc2ab4465
Ethan Elkayam <ethanelkaim17@gmail.com>
2025-05-08 14:31:40 +0300

    a

--- engine/trainer.py
+++ engine/trainer.py
@@ -207,7 +207,7 @@ def setup(rank, world_size):
     os.environ["MASTER_ADDR"] = "localhost"
     os.environ["MASTER_PORT"] = "12355"
     
-    print("rank")
+    print(rank)
     torch.cuda.set_device(rank)
 
     dist.init_process_group(backend="nccl", 
