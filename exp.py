a0ac385b6204f12621a136ef8b0e4f92c1ad5639
Ethan Elkayam <ethanelkaim17@gmail.com>
2025-05-10 22:26:26 +0300

    a

--- exp.py
+++ exp.py
@@ -16,10 +16,14 @@ TRAIN_DIR = "~/data"
 
 
 train_dataset = RexailDataset(root=TRAIN_DIR,
-                                        transform=None,
-                                        pre_transform=None,
-                                        complement_ratio=False,
-                                        storewise=True)
+                                transform=None,
+                                pre_transform=None,
+                                class_decider=partial(RexailDataset.filter_by_min,
+                                                        threshold=500),
+                                max_class_size=500,
+                                ratio=90,
+                                complement_ratio=True,
+                                storewise=True)
 
 print("calculating")
 
